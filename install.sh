1da5384ed9b3577d6383b46de03b69cad04c41e7
Brandon Crowe <bcrowe@crowe-cloud.com>
2018-12-05 09:22:29 -0500

    Comments install script

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+
+# Make program install directory
 mkdir /usr/local/flowvizer-edge
+
+# Make program config directory
 mkdir /etc/flowvizer-edge/
+
+# Copy the service file to systemd
 cp -f ./flowvizer-edge.service /etc/systemd/system/
+
+# Copy content of tar to install directory
 cp -rf ./* /usr/local/flowvizer-edge/
+
+# Make changes to content in install directory
 cd /usr/local/flowvizer-edge
 mv ./livego ./flowvizer-edge
 chmod +x ./flowvizer-edge
+
+# Reload system services
 systemctl daemon-reload
+
+# Start the Service
 systemctl start flowvizer-edge
